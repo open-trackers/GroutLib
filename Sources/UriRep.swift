5f41fa75a69440ac3720162270433cf2dfdb2e61
Reed Es <reedes@fastmail.com>
2022-12-18 10:26:07 -0700

    ongoing

--- Sources/UriRep.swift
+++ Sources/UriRep.swift
@@ -18,44 +18,44 @@ import Foundation
 /// Identifiable for use in .sheet and .fullScreenCover.
 /// RawRepresentable so that it can be stored in SceneStorage or AppStorage.
 /// Typed for use as a navigationDestination.
-public struct UriRep<T>: Hashable, Codable, Identifiable, RawRepresentable {
-    public let value: URL
-
-    public init(value: URL) {
-        self.value = value
-    }
-
-    public var id: Int { value.hashValue }
-
-    enum CodingKeys: String, CodingKey {
-        case value
-    }
-
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        value = try values.decode(URL.self, forKey: .value)
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(value, forKey: .value)
-    }
-
-    public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-              let result = try? JSONDecoder().decode(UriRep.self, from: data)
-        else {
-            return nil
-        }
-        self = result
-    }
-
-    public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-              let string = String(data: data, encoding: .utf8)
-        else {
-            return "{}"
-        }
-        return string
-    }
-}
+//public struct UriRep<T>: Hashable, Codable, Identifiable, RawRepresentable {
+//    public let value: URL
+//
+//    public init(value: URL) {
+//        self.value = value
+//    }
+//
+//    public var id: Int { value.hashValue }
+//
+//    enum CodingKeys: String, CodingKey {
+//        case value
+//    }
+//
+//    public init(from decoder: Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        value = try values.decode(URL.self, forKey: .value)
+//    }
+//
+//    public func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(value, forKey: .value)
+//    }
+//
+//    public init?(rawValue: String) {
+//        guard let data = rawValue.data(using: .utf8),
+//              let result = try? JSONDecoder().decode(UriRep.self, from: data)
+//        else {
+//            return nil
+//        }
+//        self = result
+//    }
+//
+//    public var rawValue: String {
+//        guard let data = try? JSONEncoder().encode(self),
+//              let string = String(data: data, encoding: .utf8)
+//        else {
+//            return "{}"
+//        }
+//        return string
+//    }
+//}
