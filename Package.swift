0b7bcb08f2267c737f2e64891fe086609f4a7152
Reed Es <reedes@fastmail.com>
2023-03-30 19:01:40 -0600

    Moved TextFieldPreset into its own OpenAlloc package

--- Package.swift
+++ Package.swift
@@ -12,12 +12,14 @@ let package = Package(name: "GroutLib",
                       dependencies: [
                           .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.4"),
                           .package(url: "https://github.com/open-trackers/TrackerLib.git", from: "1.0.0"),
+                          .package(url: "https://github.com/openalloc/SwiftTextFieldPreset.git", from: "1.0.0"),
                       ],
                       targets: [
                           .target(name: "GroutLib",
                                   dependencies: [
                                       .product(name: "Collections", package: "swift-collections"),
                                       .product(name: "TrackerLib", package: "TrackerLib"),
+                                      .product(name: "TextFieldPreset", package: "SwiftTextFieldPreset"),
                                   ],
                                   path: "Sources"),
                           .testTarget(name: "GroutLibTests",
